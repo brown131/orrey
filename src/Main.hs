ed676d2801fe04349427f97e63afd512d9850e35
Scott Brown <brown131@yahoo.com>
2020-09-19 16:22:10 -0500

    Planet motion

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ import Data.Astro.Planet
 import Data.Astro.Planet.PlanetDetails
 import Data.Astro.Time.JulianDate()
 import Data.Astro.Types
+import Data.Fixed
 import Data.List()
 import Data.Maybe()
 import Data.Time
@@ -35,48 +36,71 @@ planets = [Mercury, Venus, Earth, Mars, Jupiter, Uranus, Neptune]
 
 data OrreyState = OrreyState {
   osEpoch :: UTCTime,
-  osPlanets :: [PlanetInfo],
-  osTicks :: Int
+  osJD :: Double,
+  osPlanets :: [PlanetInfo]
   }
 
 initialState :: IO OrreyState
 initialState = do now <- getCurrentTime
                   return $ OrreyState {
                     osEpoch = now,
-                    osPlanets = map (\p -> planetInfo p) planets,
-                    osTicks = 0
+                    osJD = 0,
+                    osPlanets = map (\p -> planetInfo p) planets
                     }
   
 type InputEvent = G.Event
 
--- renderEllipse :: Rectangle -> Picture
--- renderEllipse r = (rectPos r)
--- rectSize r
+-- TODO: This is not accounting for the anomoly at the epoch.
+trueAnomaly :: Planet -> Double -> Double
+trueAnomaly p jd = mod' (jd * rad / tp) rad
+                   where rad = 2 * pi
+                         tpe = 365.24255 * (pdTp $ j2010PlanetDetails Earth)
+                         tp = (pdTp $ j2010PlanetDetails p) * tpe
 
+-- TODO: This is not accounting for ecliptic lat/long.
+planetCoordinates :: Planet -> Double -> (Float, Float)
+planetCoordinates p v = (realToFrac x, realToFrac y)
+                        where a = auToKM $ pdAlpha $ j2010PlanetDetails p
+                              au = auToKM $ pdAlpha $ j2010PlanetDetails Earth
+                              e = pdE $ j2010PlanetDetails p
+                              r = (a / au) * (1 - e^2) / (1 + e * cos v)
+                              x = r * cos v
+                              y = r * sin v
+                                                 
 movePlanets :: Maybe InputEvent -> OrreyState -> OrreyState
-movePlanets (Just (G.EventKey (G.MouseButton G.LeftButton) G.Down _ _)) st =
-  st { osTicks = (+1) $ osTicks st }
-movePlanets _ st = st
+movePlanets (Just (G.EventKey (G.MouseButton G.LeftButton) G.Down _ _)) st = st
+movePlanets _ st = st { osJD = (+1) $ osJD st }
 
 renderStatusBar :: OrreyState -> [Picture]
 renderStatusBar st = [Translate (-450) (-390) $ Color white $
                        Polygon [(0, 0), (0, 30), (900, 30), (900, 0)]] ++
-                     [Translate (-440) (-380) $ Scale 0.1 0.1 $ Text $ ("Ticks: " ++ (show $ osTicks st))]
+                     [Translate (-440) (-380) $ Scale 0.1 0.1 $ Text $
+                      ("Earth: " ++ (show x)) ++ "," ++ (show y) ++
+                      " v=" ++ (show $ trueAnomaly Earth jd)]
+                     where jd = osJD st
+                           (x, y) = planetCoordinates Earth $ trueAnomaly Earth jd
+                           au = realToFrac $ auToKM $ pdAlpha $ j2010PlanetDetails Earth
 
 renderSun :: [Picture]
 renderSun = [Translate 0 0 $ Color yellow $ ThickCircle 1 8]
 
+renderPlanet :: Planet -> Double -> Picture
+renderPlanet p jd = Translate x' y' $ Color color $ ThickCircle 1 size
+                    where (x, y) = planetCoordinates p (trueAnomaly p jd)
+                          scale = 70
+                          x' = x * scale
+                          y' = y * scale
+                          color = piColor $ planetInfo p
+                          size = piSize $ planetInfo p
+       
 renderOrbit :: Planet -> Picture
 renderOrbit p = Translate 0 0 $ Color white $ Circle (aus * 70)
-                where aus = realToFrac $ pdAlpha (j2010PlanetDetails p)
-
--- renderPlanet :: Planet -> OrreyState -> Picture
--- renderPlanet p st = []
+                where aus = realToFrac $ pdAlpha $ j2010PlanetDetails p
 
 renderOrrey :: OrreyState -> Picture 
 renderOrrey st = Pictures $ renderSun ++
-                 map (\p -> renderOrbit p) planets ++
-                 [Translate 0 0 $ Color red $ Arc 45 90 100] ++
+                 -- map (\p -> renderOrbit p) planets ++
+                 map (\p -> renderPlanet p (osJD st)) planets ++
                  renderStatusBar st
 
 network :: SignalGen (Signal (Maybe Float))
